83bb6986c6c11796bfa7ebedcd62a5d8710e1b9d
SAQIB BILAL <44872127+saqibb@users.noreply.github.com>
2022-09-01 16:05:43 +0400

    Add files via upload

---
+++ qr_code.py
@@ -0,0 +1,11 @@
+# Track phone number
+#### pip install pyqrcode 
+import pyqrcode
+# pip install pypng
+import png
+# link variable
+link = "https://github.com/saqibb/python/"
+# Create Link qrcode
+qrCode = pyqrcode.create(link)
+# convert to png
+qrCode.png("link.png",scale=5)
