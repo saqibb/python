34e15f2e309117c182b1496ab462f2cbe61ec274
SAQIB BILAL <44872127+saqibb@users.noreply.github.com>
2022-09-01 21:37:38 +0400

    Add files via upload

---
+++ screenshot.py
@@ -0,0 +1,12 @@
+# Screenshot 
+#### pip install pyautogui 
+
+import pyautogui
+
+# get Screenshot
+
+screenshot = pyautogui.screenshot()
+
+# save screenshot 
+
+screenshot.save(r'D:\python\snippets\screenshot.png')
